bd18f7dc0ba8ec13d12cdfa9100f7f4c145f72b9
steven-kollo <steven.kollo.workspace@gmail.com>
2025-05-10 20:18:14 +0400

    temp origins tests only

--- flask/app.py
+++ flask/app.py
@@ -19,9 +19,16 @@ app.secret_key = os.getenv("APP_SECRET_KEY")
 CORS(app,
     supports_credentials=True,
     origins=[
+        "https://caretaker.needleetools.com",
+        "https://echo.needleetools.com",
+        "https://hub.needleetools.com",
         "https://caretaker.needlee.ai",
         "https://echo.needlee.ai",
         "https://hub.needlee.ai",
+        "http://localhost:3000", 
+        "http://127.0.0.1:3000", 
+        "http://localhost", 
+        "http://127.0.0.1"
     ],
     allow_headers=["Content-Type"],
     methods=["GET", "POST", "OPTIONS"])
